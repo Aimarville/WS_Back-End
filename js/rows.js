0635e61b65e63b4a7be5ab3665b1e2ad4719e380
Aimar <aesparza014@ikasle.ehu.eus>
2025-11-17 12:41:35 +0100

    Seccond milestone 1,2,3 zatiak

---
+++ js/rows.js
@@ -0,0 +1,91 @@
+// YOUR CODE HERE :
+// .... stringToHTML ....
+// .... setupRows .....
+
+const delay = 350;
+const attribs = ['nationality', 'leagueId', 'teamId', 'position', 'birthdate']
+
+
+let setupRows = function (game) {
+
+
+    function leagueToFlag(leagueId) {
+        // YOUR CODE HERE
+    }
+
+
+    function getAge(dateString) {
+        const today = new Date();
+        const birthDate = new Date(dateString);
+        let age = today.getFullYear() - birthDate.getFullYear();
+        const monthDiff = today.getMonth() - birthDate.getMonth();
+        const dayDiff = today.getDate() - birthDate.getDate();
+        if (monthDiff < 0 || (monthDiff === 0 && dayDiff < 0)) {
+            age--;
+        }
+        return age;
+    }
+
+    let check = function (theKey, theValue) {
+        if (theKey === "birthdate") {
+            const solutionAge = getAge(game.solution[theKey]);
+            const userAge = getAge(theValue);
+
+            if (solutionAge === userAge) return "correct";
+            if (solutionAge > userAge) return "higher";
+            if (solutionAge < userAge) return "lower";
+        }
+        if (game.solution[theKey] === theValue) {
+            return "correct";
+        }
+
+        return "incorrect";
+    }
+
+    function setContent(guess) {
+        return [
+            `<img src="https://playfootball.games/media/nations/${guess.nationality.toLowerCase()}.svg" alt="" style="width: 60%;">`,
+            `<img src="https://playfootball.games/media/competitions/${leagueToFlag(guess.leagueId)}.png" alt="" style="width: 60%;">`,
+            `<img src="https://cdn.sportmonks.com/images/soccer/teams/${guess.teamId % 32}/${guess.teamId}.png" alt="" style="width: 60%;">`,
+            `${guess.position}`,
+            `${getAge(guess.birthdate)}`
+        ]
+    }
+
+    function showContent(content, guess) {
+        let fragments = '', s = '';
+        for (let j = 0; j < content.length; j++) {
+            s = "".concat(((j + 1) * delay).toString(), "ms")
+            fragments += `<div class="w-1/5 shrink-0 flex justify-center ">
+                            <div class="mx-1 overflow-hidden w-full max-w-2 shadowed font-bold text-xl flex aspect-square rounded-full justify-center items-center bg-slate-400 text-white ${check(attribs[j], guess[attribs[j]]) == 'correct' ? 'bg-green-500' : ''} opacity-0 fadeInDown" style="max-width: 60px; animation-delay: ${s};">
+                                ${content[j]}
+                            </div>
+                         </div>`
+        }
+
+        let child = `<div class="flex w-full flex-wrap text-l py-2">
+                        <div class=" w-full grow text-center pb-2">
+                            <div class="mx-1 overflow-hidden h-full flex items-center justify-center sm:text-right px-4 uppercase font-bold text-lg opacity-0 fadeInDown " style="animation-delay: 0ms;">
+                                ${guess.name}
+                            </div>
+                        </div>
+                        ${fragments}`
+
+        let playersNode = document.getElementById('players')
+        playersNode.prepend(stringToHTML(child))
+    }
+
+    let getPlayer = function (playerId) {
+        // YOUR CODE HERE
+        return game.players.find(player => player.id === playerId);
+    }
+
+    return /* addRow */ function (playerId) {
+
+        let guess = getPlayer(playerId)
+        console.log(guess)
+
+        let content = setContent(guess)
+        showContent(content, guess)
+    }
+}
