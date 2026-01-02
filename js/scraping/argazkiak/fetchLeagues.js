0a2d793066dfed7c9983c87106bf94cfb2f5cf46
AimarV <avillegas016@ikasle.ehu.eus>
2026-01-02 20:29:11 +0100

    Milestone1: Ligak lortzen

---
+++ js/scraping/argazkiak/fetchLeagues.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const fsP = require('fs/promises');
+const fs = require('fs');
+const fetch = require('node-fetch');
+
+const writepath = path.join(__dirname, '../../../public/images/leagues');
+console.log(writepath);
+
+async function main() {
+    try {
+        // create directoy
+        await fsP.mkdir(writepath, { recursive: true });
+
+        // read leagues file into an array of lines
+        const content = await fsP.readFile(path.join(__dirname, "leagues.txt"), "utf8");
+        const data = content.split("\r\n");
+
+        console.log(data);
+
+        data.forEach( (elem, idx) => {
+            const url = `https://playfootball.games/media/competitions/${elem}.png`
+
+            console.log(`${elem}.png`);
+
+            fetch(url)
+                .then(res => {
+                    // check status
+                    if (res.status === 200) {
+                        const fileStream = fs.createWriteStream(path.join(writepath, `${elem}.png`));
+                        res.body.pipe(fileStream);
+                    } else {
+                        console.log(`status: ${res.status} line: ${idx} elem: ${elem} not found`)
+                    }
+                })
+                .catch(err => console.log(err))
+        })
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+main();
